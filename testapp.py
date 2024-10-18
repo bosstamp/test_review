eccede8e94eddb67c8b8b99d924f7d04a601161c
bosstamp <bosstampklom@gmail.com>
2024-10-19 01:04:27 +0700

    Update testapp.py
    hidden mainmenu

--- testapp.py
+++ testapp.py
@@ -1,18 +1,9 @@
 import pandas as pd
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+#MainMenu {
+  visibility: hidden;
+}
 
 # Predefined mappings for h4cd and h4nm
 df_dict = pd.read_csv('test_class.csv')
